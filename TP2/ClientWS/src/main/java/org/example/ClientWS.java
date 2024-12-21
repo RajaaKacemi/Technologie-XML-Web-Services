8da19b8ede5af72dbcd88d571d25f7fcb4bd2131
RajaaKacemi <rajaa01.kacemi@gmail.com>
2024-12-21 09:51:03 +0100

    done

--- TP2/ClientWS/src/main/java/org/example/ClientWS.java
+++ TP2/ClientWS/src/main/java/org/example/ClientWS.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import proxy.BanqueService;
+import proxy.BanqueWS;
+import proxy.Compte;
+
 public class ClientWS {
     public static void main(String[] args) {
         BanqueService proxy = new BanqueWS().getBanqueServicePort();
-        System.out.println(proxy.ConversionEuroToDH(10));
-
+        System.out.println(proxy.conversionEuroToDH(10));
+        Compte cp = proxy.getCompte(1);
+        System.out.println(cp.getCode());
+        System.out.println(cp.getDateCreation());
     }
 }
